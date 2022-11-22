d5e28e4ebe99ec8636e6888fa34fa5e945edeb66
Lucas Simionato <simionato.lucas99@gmail.com>
2022-11-22 14:22:55 -0300

    added drawing and erasing

--- main.js
+++ main.js
@@ -1,13 +1,40 @@
-const squaresContainer = document.querySelector("#squares-container");
+function createGrid(size) {
+    const squaresContainer = document.querySelector("#squares-container");
 
-for (let i = 0; i < 16; i++) {
-    for (let j = 0; j < 16; j++) {
-        const div = document.createElement("div");
-        div.classList.add("square");
-        squaresContainer.appendChild(div);
+    for (let i = 0; i < size; i++) {
+        for (let j = 0; j < size; j++) {
+            const div = document.createElement("div");
+            div.classList.add("square");
+            squaresContainer.appendChild(div);
+        }
+
+        const lineBreak = document.createElement("div");
+        lineBreak.classList.add("line-break");
+        squaresContainer.appendChild(lineBreak);
     }
-
-    const lineBreak = document.createElement("div");
-    lineBreak.classList.add("line-break");
-    squaresContainer.appendChild(lineBreak);
 }
+
+createGrid(16);
+
+const squares = document.querySelectorAll(".square");
+
+squares.forEach(square => square.addEventListener("mouseover", (e) => {
+    if (e.buttons == 1) {
+        square.classList.add("selected");
+    } else if (e.buttons == 2) {
+        square.classList.remove("selected");
+    }
+}));
+
+squares.forEach(square => square.addEventListener("click", (e) => {
+    if (e.buttons == 0) {
+        square.classList.add("selected");
+    } else if (e.buttons == 2) {
+        square.classList.remove("selected");
+    }
+}));
+
+const resetCanvas = document.querySelector("#reset-canvas");
+resetCanvas.addEventListener("click", () => {
+    squares.forEach(square => square.classList.remove("selected"));
+});
