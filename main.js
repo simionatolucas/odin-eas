1985d22dcab40a3d011057795f3831d25a6d187b
Lucas Simionato <simionato.lucas99@gmail.com>
2022-11-24 10:20:36 -0300

    added variable grid size
    Added a prompt for the user to input a custom grid size. Refactored
    to use css grid instead of flexbox.

--- main.js
+++ main.js
@@ -1,40 +1,61 @@
 function createGrid(size) {
     const squaresContainer = document.querySelector("#squares-container");
 
+    squaresContainer.setAttribute('style', `grid-template-columns: repeat(${size}, 1fr); 
+                                            grid-template-rows: repeat(${size}, 1fr);`);  
+
     for (let i = 0; i < size; i++) {
         for (let j = 0; j < size; j++) {
             const div = document.createElement("div");
             div.classList.add("square");
             squaresContainer.appendChild(div);
         }
-
-        const lineBreak = document.createElement("div");
-        lineBreak.classList.add("line-break");
-        squaresContainer.appendChild(lineBreak);
     }
 }
 
 createGrid(16);
+enable();
 
-const squares = document.querySelectorAll(".square");
+const gridSizeBtn = document.querySelector("#grid-size");
+gridSizeBtn.addEventListener("click", () => {
+    let size = prompt("Grid size (max 100): ");
+    let currentSize = document.querySelector("#current-size");
 
-squares.forEach(square => square.addEventListener("mouseover", (e) => {
-    if (e.buttons == 1) {
-        square.classList.add("selected");
-    } else if (e.buttons == 2) {
-        square.classList.remove("selected");
+    while(size > 100) {
+        size = prompt("Grid size (max 100): ");
     }
-}));
 
-squares.forEach(square => square.addEventListener("click", (e) => {
-    if (e.buttons == 0) {
-        square.classList.add("selected");
-    } else if (e.buttons == 2) {
-        square.classList.remove("selected");
-    }
-}));
+    resetCanvas();
+    createGrid(size);
+    enable();
 
-const resetCanvas = document.querySelector("#reset-canvas");
-resetCanvas.addEventListener("click", () => {
-    squares.forEach(square => square.classList.remove("selected"));
+    currentSize.textContent = `Current size: ${size}`;
 });
+
+
+function enable() {
+    const squares = document.querySelectorAll(".square");
+    squares.forEach(square => square.addEventListener("mouseover", (e) => {
+        if (e.buttons == 1) {
+            square.classList.add("selected");
+        } else if (e.buttons == 2) {
+            square.classList.remove("selected");
+        }
+    }));
+    
+    squares.forEach(square => square.addEventListener("mousedown", (e) => {
+        if (e.buttons == 1) {
+            square.classList.add("selected");
+        } else if (e.buttons == 2) {
+            square.classList.remove("selected");
+        }
+    }));
+}
+
+const resetCanvasBtn = document.querySelector("#reset-canvas");
+resetCanvasBtn.addEventListener("click", resetCanvas);
+
+function resetCanvas() {
+    const squares = document.querySelectorAll(".square");
+    squares.forEach(square => square.classList.remove("selected"));
+}
